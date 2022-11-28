aee473c3f728c04f0c196bf0d9eedee527e45aac
dargor980 <german.contrerasa@utem.cl>
2022-11-27 21:08:09 -0300

    ADD: Tiendas chilenas e interfaz traducida al español

---
+++ js/background.js
@@ -0,0 +1,134 @@
+// The active background music track is stored here instead of themeAudio.src
+var currentMusic = ''
+var musicEnabled = true
+
+// Set MediaSession API info for Chrome media player popup
+if ('mediaSession' in navigator) {
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: 'Wii Shop Channel Music Extension'
+    })
+}
+
+// Creat audio object
+var themeAudio = new Audio()
+themeAudio.volume = 0.5
+themeAudio.loop = true
+
+// Get stored settings
+chrome.storage.local.get({
+    music: 'wii-shop-theme',
+    musicEnabled: true
+}, function (data) {
+    currentMusic = chrome.extension.getURL('music/' + data.music + '.ogg')
+    console.log('Music enabled:', data.musicEnabled)
+    musicEnabled = data.musicEnabled
+})
+
+// Update settings after storage change
+chrome.storage.onChanged.addListener(function (changes, area) {
+    if (changes.musicEnabled) {
+        musicEnabled = changes.musicEnabled.newValue
+        if (!changes.musicEnabled) {
+            themeAudio.src = ''
+        }
+    }
+    if (changes.music) {
+        currentMusic = chrome.extension.getURL('music/' + changes.music.newValue + '.ogg')
+        if (musicEnabled) {
+            themeAudio.src = chrome.extension.getURL('music/' + changes.music.newValue + '.ogg')
+            themeAudio.play()
+        }
+    }
+})
+
+// Function for checking if music should be playing in current tab
+function checkMusic(tabs) {
+    var url = new URL(tabs[0].url)
+    var domain = url.hostname.toString().replace('www.', '')
+    if (siteList.includes(domain) && musicEnabled) {
+        if (themeAudio.src != currentMusic) {
+            themeAudio.src = currentMusic
+        }
+        themeAudio.play()
+    } else {
+        // The source value is deleted so Chromium browsers won't show a playback notification anymore
+        themeAudio.src = ''
+    }
+}
+
+// Detect new page loads in active tab, if the domain matches a shopping site, play music
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+    if (changeInfo.status === 'complete') {
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+            checkMusic(tabs)
+        })
+    }
+})
+
+// Detect shopping tab becoming inactive/closed, if the domain matches a shopping site, play music
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+        checkMusic(tabs)
+    })
+})
+
+// Listen for pause/play commands from popup
+chrome.runtime.onMessage.addListener(function (request) {
+    if (request === 'pause') {
+        themeAudio.src = ''
+    } else if (request === 'play') {
+        themeAudio.src = currentMusic
+        themeAudio.play()
+    }
+})
+
+// Show notification on extension install
+chrome.runtime.onInstalled.addListener(function () {
+    // Set most options
+    var data = {
+        'type': 'basic',
+        'iconUrl': chrome.extension.getURL('img/icon128.png'),
+        'title': 'Wii Shop Channel Music - Chilean Edition instalada!!',
+    }
+    // Set message and handlers for notification
+    if (navigator.userAgent.includes("Firefox")) {
+        // Firefox supports does not support buttons in notifications
+        data.message = 'Ahora podrás escuchar el tema de Wii Shop mientras  visitas tus páginas web de compras, haz click en la barra de tareas para modificar la configuración, o bien haz click en esta notificación.'
+        handleNotif = function (id) {
+            chrome.notifications.onClicked.addListener(function (id) {
+                chrome.windows.create({
+                    'url': chrome.extension.getURL('popup.html'),
+                    'width': 300,
+                    'height': 500,
+                    'type': 'popup'
+                })
+            })
+        }
+    } else {
+        // Chromium browsers don't support openPopup(), but do support a button
+        data.message = 'Ahora podrás escuchar el tema de Wii Shop mientras  visitas tus páginas web de compras, haz click en la barra de tareas para modificar la configuración, o bien haz click en esta notificación.'
+        data.buttons = [{
+            title: 'Abrir Configuración'
+        },
+        {
+            title: 'Ver github'
+        }
+        ]
+        handleNotif = function (id) {
+            chrome.notifications.onButtonClicked.addListener(function (id, i) {
+                if (i === 0) {
+                    chrome.windows.create({
+                        'url': chrome.extension.getURL('popup.html'),
+                        'width': 300,
+                        'height': 500,
+                        'type': 'popup'
+                    })
+                } else if (i === 1) {
+                    chrome.tabs.create({ url: 'https://github.com/dargor980' })
+                }
+            })
+        }
+    }
+    // Display the notification
+    chrome.notifications.create(data, handleNotif)
+})
---
+++ js/popup.js
@@ -0,0 +1,54 @@
+// Save settings
+document.querySelector('#music-picker').addEventListener('change', function () {
+    chrome.storage.local.set({
+        music: document.querySelector('#music-picker').value
+    })
+})
+
+// Get stored settings
+chrome.storage.local.get({
+    music: 'wii-shop-theme',
+    musicEnabled: 'true'
+}, function (data) {
+    document.querySelector('#music-picker').value = data.music
+    if (data.musicEnabled) {
+        document.getElementById('music-toggle').innerText = 'Apagar música de fondo'
+    } else {
+        document.getElementById('music-toggle').innerText = 'Encender música de fondo'
+    }
+})
+
+// Music on/off button
+document.getElementById('music-toggle').addEventListener('click', function() {
+    chrome.storage.local.get({
+        musicEnabled: true
+    }, function (data) {
+        console.log(data)
+        if (data.musicEnabled) {
+            // Turn off music
+            document.getElementById('music-toggle').innerText = 'Encender música de fondo'
+            chrome.storage.local.set({
+                musicEnabled: false
+            })
+        } else {
+            // Turn on music
+            document.getElementById('music-toggle').innerText = 'Apagar música de fondo'
+            chrome.storage.local.set({
+                musicEnabled: true
+            })
+        }
+    })
+})
+
+// Button link functionality
+document.querySelectorAll('button[data-link]').forEach(function (el) {
+    el.addEventListener('click', function () {
+        chrome.tabs.create({ url: el.getAttribute('data-link') })
+    })
+})
+
+// Pause music when page closes
+// This only works on the popup opened from the notification, not the browserAction button
+window.addEventListener("beforeunload", function (e) {
+    chrome.runtime.sendMessage('pause')
+}, false)
